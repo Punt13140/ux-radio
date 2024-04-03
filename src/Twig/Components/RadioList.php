ae305e26832ec2e5fa80ab90fef04fc964c20434
NICOLAU Bastien <bastien.nicolau@univ-amu.fr>
2024-04-03 09:50:56 +0200

    Create RadioList component

---
+++ src/Twig/Components/RadioList.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Repository\RadioRepository;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class RadioList
+{
+    public ?string $filter = null;
+
+    public function __construct(
+        private readonly RadioRepository $radioRepository
+    )
+    {
+    }
+
+    public function getRadios(): array
+    {
+        if (null === $this->filter) {
+            return $this->radioRepository->findAll();
+        }
+
+        return $this->radioRepository->findByGenre($this->filter);
+    }
+
+}
