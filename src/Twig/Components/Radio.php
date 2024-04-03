9d613ee7dac72cd65634185d750221f7b19ef89f
NICOLAU Bastien <bastien.nicolau@univ-amu.fr>
2024-04-03 11:29:39 +0200

    Create Radio component

---
+++ src/Twig/Components/Radio.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+final class Radio
+{
+
+    public \App\Entity\Radio $radio;
+    
+
+}
